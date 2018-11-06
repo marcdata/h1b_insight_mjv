2f5046c69672558e0c61fc7472b899ee2237cdd6
Marc Velasco <marcvelasco999@yahoo.com>
2018-11-05 22:40:16 -0500

    Standardized casing in unit test files.
    Code in unit test was mix of pythonic_casing_convention and camelCasing. Standardized all to camels.

--- src/insightH1bReport/insightH1bReport/Tests/ReportWriterTests.py
+++ src/insightH1bReport/insightH1bReport/Tests/ReportWriterTests.py
@@ -3,19 +3,19 @@ from ReportDataContracts import StateInfo, OccupationInfo, ReportSummaryData
 from ReportWriter import ReportWriter
 from ReportWriter import ToReportPercentFormat
 
-def test_percent_format():
+def testPercentFormat():
     relRate = 0.204
     assert ToReportPercentFormat(relRate) == '20.4%'
 
-def test_percent_format_round_down():
+def testPercentFormatRoundDown():
     relRate = 0.0104
     assert ToReportPercentFormat(relRate) == '1.0%'
 
-def test_percent_format_round_up():
+def testPercentFormatRoundUp():
     relRate = 0.0106
     assert ToReportPercentFormat(relRate) == '1.1%'
 
-def test_percent_formatter_raises_on_badinput():
+def testPercentFormatterRaisesOnBadinput():
     badRelRate = '0.123'
     with pytest.raises(ValueError):
         ToReportPercentFormat(badRelRate)
@@ -24,13 +24,13 @@ def test_percent_formatter_raises_on_badinput():
 report line format issues
 """
 
-def test_sample_state_report_line_format():
+def testSampleStateReportLineFormat():
     stateInfo = StateInfo('KS', 10, 0.0501)
     reportWriter = ReportWriter()
     sampleOutput = reportWriter.ToReportLineState(stateInfo)
     assert sampleOutput == "KS;10;5.0%"
 
-def test_sample_occupation_report_line_format():
+def testSampleOccupationReportLineFormat():
     occupationInfo = OccupationInfo('Data Engineer', 21, .1151)
     reportWriter = ReportWriter()
     sampleOutput = reportWriter.ToReportLineOccupation(occupationInfo)
